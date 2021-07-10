f88e8c83627138566ec596e5aeaf1bbc0f04a676
Johnson Nguy <johnsonnguy@Johnsons-MBP.attlocal.net>
2021-07-09 22:35:40 -0700

    Added page routings

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.index, name='new_interaction'),
+    path('home', views.home, name='home'),
+    path('settings', views.settings, name='settings'),
+    path('designs', views.designs, name='designs'),
+    path('mydesigns', views.my_designs, name='my_designs'),
+    path('splash', views.splash, name="splash")
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,22 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def home(request):
+    return render(request, 'coloring/home.html')
+
+# Search for designs page
+def designs(request):
+    return render(request, 'coloring/designs.html')
+
+# My designs page
+def my_designs(request):
+    return render(request, 'coloring/my_designs.html')
+
+def settings(request):
+    return render(request, 'coloring/settings.html')
+
+def splash(request):
+    return render(request, 'coloring/splash.html')
+
+
